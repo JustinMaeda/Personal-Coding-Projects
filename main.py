b82d45bddf31a856c6898e22d35b72d0aa1007f1
JustinMaeda <jkmaeda@hawaii.edu>
2024-09-27 18:06:00 -1000

    My first time

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-class PROFILE:
-  def __init__(self, NAME, CLASS, STATS, CURRENCY):
-    self.Name = NAME
-    self.Class = CLASS
-    self.Stats = STATS
-    self.Currency = CURRENCY
+big fat nuts
 
